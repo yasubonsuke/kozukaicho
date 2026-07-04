7fbee18eafafb1f82a7d2a0944a30b28b9deecbb
yasubonsuke <my6201bon@au.com>
2026-07-04 14:59:09 +0900

    Add files via upload

---
+++ sw 2.js
@@ -0,0 +1,26 @@
+const CACHE = "kozukaicho-v3";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit => hit || fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return res;
+    }).catch(err => {
+      if (e.request.mode === "navigate") return caches.match("./index.html");
+      throw err;
+    }))
+  );
+});
